2d74f1a286c8bce5ab1b6c22fe83e1a263c84465
hhanlin723 <hhanlin0321@gmail.com>
2024-04-20 09:32:18 -0500

    update1

--- src/components/MyRouter.js
+++ src/components/MyRouter.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage.js';
 import CityInfoPage from './pages/CityInfoPage.js';
 import NotFoundPage from './pages/NotFoundPage.js';
@@ -8,13 +8,13 @@ import UserPortal from './pages/UserPortal.js';
 
 function MyRouter() {
   return (
-    <Routes>
+    <Switch>
         <Route path='/' element={<HomePage />} />
         <Route path='/cityinfo' element={<CityInfoPage />} />
         <Route path='/city/:id' element={<CityInfoPage />} /> {/* Route with city ID parameter */}
         <Route path='/user' element={<UserPortal />} />
         <Route path='*' element={<NotFoundPage />} />
-    </Routes>
+    </Switch>
   );
 }
 
--- src/components/pages/CityInfoPage.js
+++ src/components/pages/CityInfoPage.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { AppContext } from '../store/AppContext.js';
-import DestinationCard from '../components/DestinationCard.js'; // Import DestinationCard component
+import { AppContext } from '../../store/AppContext.js'
+import DestinationCard from '../../components/DestinationCard.js'; // Import DestinationCard component
 
 function CityInfoPage() {
     const { app } = useContext(AppContext);
